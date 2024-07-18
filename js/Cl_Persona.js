2ee526369ac5e99eee75aeeb980acbb2da91e2d6
RicardoPerez13 <1001.30995733.ucla@gmail.com>
2024-07-18 10:42:11 -0400

    commit(feature)Segundo commit

--- js/Cl_Persona.js
+++ js/Cl_Persona.js
@@ -3,9 +3,16 @@ export default class Cl_Persona {
         this._nombre=nombre;
         this._sexo=sexo;
     }
-    get nombre() {
+    set nombre(no){
+        this._nombre=no;
+    }
+
+    get nombre(){
         return this._nombre;
     }
+    set sexo(s){
+        this._sexo=s;
+}
     get sexo() {
         return this._sexo;
     }
